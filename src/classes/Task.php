63dcf219a3dfb9f62b5260fdca33f03e3eb2f0d6
AnousoneM <anousone.mounivongs@gmail.com>
2025-09-03 09:27:06 +0200

    mise à jour mineure

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -36,7 +36,7 @@ class Task
     public function __toString()
     {
         // je fais en sorte de return ma structure html / css.
-        // je ne vais pas m'en servir dans le TD.
+        // je ne vais pas m'en servir dans ce TD :p
         return '<div class="my-2">
                  <a href="action.php?action=add" class="btn btn-outline-primary">' . $this->title . '<i class="bi bi-check"></i></a>
                  <a href="action.php?action=delete" class="btn btn-danger"><i class="bi bi-trash3-fill"></i></a>
